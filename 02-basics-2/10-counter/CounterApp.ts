f8f6ac5855344d728938c4270a74ec92f375d415
Nikita Plyusnin <me@plyusnin.dev>
2025-06-20 17:47:32 +0300

    Resolve counter

--- 02-basics-2/10-counter/CounterApp.ts
+++ 02-basics-2/10-counter/CounterApp.ts
@@ -1,9 +1,16 @@
-import { defineComponent } from 'vue'
+import type { Ref } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const counter: Ref<number> = ref(0)
+
+    return {
+      counter,
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +18,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        @click="counter--"
+        :disabled="counter <= 0"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ counter }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        @click="counter++"
+        :disabled="counter >= 5"
       >➕</button>
     </div>
   `,
